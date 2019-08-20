def0e94e77d16e34ce8a4efe4a39ac71e5700476
Jonathan Scaramal <jonathanscaramal@MacBook-Pro-de-Jonathan.local>
2019-08-19 21:44:38 -0300

    Testing v-model, v-on:keypress and v-for with basic todo list functionality

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import List from "./views/List.vue";
 
 Vue.use(Router);
 
@@ -16,10 +17,11 @@ export default new Router({
     {
       path: "/list",
       name: "list",
+      component: List
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ "./views/List.vue")
+      // component: () => import(/* webpackChunkName: "about" */ "./views/List.vue")
     }
   ]
 });
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="list">
     <h1>Lista de tareas pendientes (o por hacer)</h1>
+    <input v-model="newTask" type="text" v-on:keypress="inputKeypress" />
+    <button v-on:click="addTask">+</button>
+    <ul id="example-1">
+      <li v-for="task in tasks" v-bind:key="task.id">{{ task.text }}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+let lastId = 0;
+
+export default {
+  name: "list",
+  data: () => ({
+    newTask: "",
+    tasks: [{ id: 0, text: "Primera tarea", checked: false }]
+  }),
+  methods: {
+    addTask() {
+      this.tasks.push({ id: ++lastId, text: this.newTask, checked: false });
+      this.newTask = "";
+    },
+    inputKeypress(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        this.addTask();
+      }
+    }
+  }
+};
+</script>
